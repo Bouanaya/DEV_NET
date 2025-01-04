e5e689500c41ae342aa8d698328b34a4513dc20d
Bouanaya <bouanayasoufiane@gmail.com>
2025-01-04 11:56:20 +0100

    Add initial configuration, database setup, and author dashboard layout

---
+++ src/config/Connexion.php
@@ -0,0 +1,37 @@
+<?php
+namespace src\Config;
+require_once __DIR__ . '/../../vendor/autoload.php';
+use Dotenv\Dotenv;
+use PDO;
+use PDOException;
+
+class Connexion {
+    private static $conn;
+
+    public static function connection() {
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+        $dotenv->load();
+
+        try {
+            self::$conn = new PDO(
+                "mysql:host=" . $_ENV["DB_HOST"] . ";dbname=" . $_ENV["DB_NAME"],
+                $_ENV["DB_USER"],
+                $_ENV["DB_PASS"]
+            );
+            self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+            // return self::$conn;
+        } catch (PDOException $th) {
+            die("Connection failed: " . $th->getMessage());
+        }
+
+        return self::$conn;
+
+    }
+
+}
+
+
+$db = new Connexion();
+$conn = $db->connection();
+
